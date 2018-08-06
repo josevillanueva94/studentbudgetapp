d874ab1376f41f2e194594f95b95c139d498bfad
Rajan Khullar <rkhullar03@gmail.com>
2018-08-06 17:39:33 -0400

    first commit

---
+++ budgetapp/models.py
@@ -0,0 +1,25 @@
+from django.db.models import IntegerField, CharField, DecimalField, TextField, DateTimeField
+from django.db.models import Model, ForeignKey, CASCADE
+from django.contrib.auth.models import User
+
+
+class Product(Model):
+    name = CharField(max_length=200)
+    unit_price = DecimalField(decimal_places=2, max_digits=10)
+    description = TextField(null=True, blank=True)
+
+
+class Budget(Model):
+    owner = ForeignKey(User, on_delete=CASCADE)
+    created = DateTimeField(auto_now=True)
+    updated = DateTimeField(auto_now=True)
+
+    # TODO: add method to get total
+
+
+class BudgetLine(Model):
+    product = ForeignKey(Product, on_delete=CASCADE)
+    quantity = IntegerField(default=1)
+    budget = ForeignKey(Budget, on_delete=CASCADE)
+
+    # TODO: add method to get subtotal
